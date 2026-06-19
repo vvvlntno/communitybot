e57c538f28eba1937e254503bc8611b53f3d3731
vvvlntno <vvvlntno@gmail.com>
2026-06-19 15:09:33 +0200

    feat(drive_topic): added drive topic bare bones

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ openai = OpenAI(api_key=api_key, base_url="https://api.mistral.ai/v1")
 
 GUILD_ID: int = int(os.getenv("GUILD_ID"))
 
-MODEL = "mistral-small-latest"
+MODEL = "mistral-medium-latest"
 TEMPERATURE = 0.7
-MAX_TOKENS = 100
+MAX_TOKENS = 1000
 SYSTEM_PROMPT = """
 You are a discord bot in an overwatch community discord.
 Your role is to answer peoples question about the game.
@@ -25,8 +25,8 @@ The user message is given in the following format:
 USER_NAME: MESSAGE"""
 
 DATABASE_PROMPT = """
-In the next message will get the database in the following simple yaml format:
-````
+In the next message you will get the database in the following simple yaml format:
+```
 USER_NAME:
 - INTEREST1
 - INTEREST2
@@ -78,6 +78,7 @@ class Client(commands.Bot):
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = discord.Client(intents=intents)
 
@@ -130,6 +131,64 @@ async def add_interest(interaction: discord.Interaction, game: str):
         + game
         + "I've marked that down <3"
     )
+
+
+@client.tree.command(
+    name="drive-topic",
+    description="Add a new topic to the server!",
+    guild=discord.Object(id=GUILD_ID),
+)
+async def drive_topic(interaction: discord.Interaction, topic: str):
+    DRIVE_TOPIC_PROMPT = DATABASE_PROMPT + """
+    And return the usernames and given topic the following format format:
+    TOPIC;USER_NAME,USER_NAME2,...
+    """
+    message_history.append(
+        {"role": "user", "content": f"{interaction.user}: {topic}"}
+    )
+    message_history.append({"role": "system", "content": DRIVE_TOPIC_PROMPT})
+    database_content = read_data()
+    message_history.append({"role": "system", "content": str(database_content)})
+
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=message_history,
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS,
+    )
+
+    reply = response.choices[0].message.content
+    topic = reply.split(";")[0]
+    user_names = reply.split(";")[1].split(",")
+
+    user_mentions = []
+    guild = client.get_guild(GUILD_ID)
+    for user_name in user_names:
+        member = guild.get_member_named(user_name)
+        if member:
+            user_mentions.append(member.mention)
+    
+    # build message
+    bot_reply = "Interested in " + topic
+    for user_mention in user_mentions:
+        bot_reply += " " + user_mention
+    
+    bot_reply += " try hitting them up!"
+
+    message_history.append({"role": "system", "content": SYSTEM_PROMPT})
+
+    # create channel
+
+    #await interaction.response.send_message(bot_reply)
+    channel = await guild.create_text_channel(name=topic)
+    await channel.send(bot_reply)
+
+    # write LLM generated message in chat
+    TEXT_CHANNEL_PROMPT = """
+    Mention people through MENTIONS and invite to to chat about TOPIC"""
+
+
+    # message_history.append({"role": "system", "content": SYSTEM_PROMPT})
 
 
 def read_data():
